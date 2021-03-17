274aa5db974171a4dd8cf832dee6c54008ea6fc8
Yushan Aisyah P.I <68349067+Yushan-Riry@users.noreply.github.com>
2021-03-18 01:35:50 +0700

    SMT4-11
    Project React Resto

---
+++ Project-React-Resto-4/project-react-resto/react-resto/src/Back/Content.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import Kategori from './Kategori';
+import Menu from './Menu';
+import Pelanggan from './Pelanggan';
+import Order from './Order';
+
+
+const Content = () => {
+    const {isi} = useParams();
+
+    let tampil;
+
+    if (isi === 'kategori') {
+        tampil = <Kategori></Kategori>
+    }
+
+    if (isi === 'menu') {
+        tampil = <Menu></Menu>
+    }
+
+    if (isi === 'pelanggan') {
+        tampil = <Pelanggan></Pelanggan>
+    }
+
+    if (isi === 'order') {
+        tampil = <Order></Order>
+    }
+
+    return (
+        <>
+            {tampil}
+        </>
+    );
+}
+
+export default Content;
